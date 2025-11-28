adf3e075154947f8d067955454786ad694e0257f
Fahad Iqbal <fahad_iqbal88@yahoo.com>
2025-11-29 02:51:57 +0800

    Add project configuration files

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Root build script
+plugins {
+    // versions aligned for stability with Compose and Serialization
+    id("com.android.application") version "8.3.2" apply false
+    kotlin("android") version "1.9.24" apply false
+    kotlin("plugin.serialization") version "1.9.24" apply false
+}
